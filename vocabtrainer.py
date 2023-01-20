5c14c2c6aff4a462cb123171fc9948636a6dba76
lukasheiling <55692511+lukasheiling@users.noreply.github.com>
2023-01-20 12:27:47 +0100

    good

--- vocabtrainer.py
+++ vocabtrainer.py
@@ -1,6 +1,6 @@
 import random
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, create_engine, text, UniqueConstraint
+from sqlalchemy import Column, Integer, String, create_engine, text, UniqueConstraint, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -12,9 +12,11 @@ Base = declarative_base()
 class Vocab(Base):
     __tablename__ = 'vocab'
     id = Column(Integer, primary_key=True)
+    unit_id = Column(Integer, ForeignKey('unit.id'))
     english = Column(String, unique=True)
     german = Column(String, unique=True)
     __table_args__ = (UniqueConstraint('english', 'german', name='uq_english_german'), )
+    
 
 class Unit(Base):
     __tablename__ = "unit"
@@ -40,36 +42,46 @@ language_choice = int(input("Enter your choice (1 or 2): "))
 session = Session()
 
 # list of units
-units = ['UNIT 1','UNIT 2','UNIT 3','UNIT 4','UNIT 5']
+unit_list = ['UNIT 1','UNIT 2','UNIT 3','UNIT 4','UNIT 5']
 
-for unit in units:
+# list of vocabs
+words_to_add = [('cat', 'Katze', 1), ('dog', 'Hund', 1), ('book', 'Buch', 2), ('house', 'Haus', 3), 
+('pig', 'Schwein', 4), ('correct', 'richtig', 5), ('mother', 'Mutter', 1), ('father', 'Vater', 5), 
+('siblings', 'Geschwister', 5), ('red', 'Rot', 4), ('green', 'Grün', 1), ('blue', 'Blau', 3), 
+('bird', 'Vogel', 5), ('cow', 'Kuh', 2), ('tree', 'Baum', 2), ('twenty', 'Zwanzig', 5), ('apple', 'Apfel', 3)]
+
+
+for unit in unit_list:
     existing_unit = session.query(Unit).filter_by(name=unit).one()
 
-
-# close the session
-
+for word in words_to_add:
+    if session.query(Vocab).filter(Vocab.english == word[0]).count() == 0:
+        session.add(Vocab(english=word[0], german=word[1], unit_id=word[2]))
 
 # Add vocabulary words to the table
-session.add(Vocab(english='cat', german='Katze'))
-session.add(Vocab(english='dog', german='Hund'))
-session.add(Vocab(english='book', german='Buch'))
-session.add(Vocab(english='house', german='Haus'))
-session.add(Vocab(english='pig', german='Schwein'))
-session.add(Vocab(english='correct', german='richtig'))
-session.add(Vocab(english='mother', german='Mutter'))
-session.add(Vocab(english='father', german='Vater'))
-session.add(Vocab(english='siblings', german='Geschwister'))
-session.add(Vocab(english='red', german='Rot'))
-session.add(Vocab(english='green', german='Grün'))
-session.add(Vocab(english='blue', german='Blau'))
-session.add(Vocab(english='bird', german='Vogel'))
-session.add(Vocab(english='cow', german='Kuh'))
-session.add(Vocab(english='tree', german='Baum'))
-session.add(Vocab(english='twenty', german='Zwanzig'))
-session.add(Vocab(english='apple', german='Apfel'))
+"""session.add(Vocab(english='cat', german='Katze', unit_id=1))
+session.add(Vocab(english='dog', german='Hund', unit_id=1))
+session.add(Vocab(english='book', german='Buch', unit_id=2))
+session.add(Vocab(english='house', german='Haus', unit_id=3))
+session.add(Vocab(english='pig', german='Schwein', unit_id=4))
+session.add(Vocab(english='correct', german='richtig', unit_id=5))
+session.add(Vocab(english='mother', german='Mutter', unit_id=1))
+session.add(Vocab(english='father', german='Vater', unit_id=5))
+session.add(Vocab(english='siblings', german='Geschwister', unit_id=5))
+session.add(Vocab(english='red', german='Rot', unit_id=4))
+session.add(Vocab(english='green', german='Grün', unit_id=1))
+session.add(Vocab(english='blue', german='Blau', unit_id=3))
+session.add(Vocab(english='bird', german='Vogel', unit_id=5))
+session.add(Vocab(english='cow', german='Kuh', unit_id=2))
+session.add(Vocab(english='tree', german='Baum', unit_id=2))
+session.add(Vocab(english='twenty', german='Zwanzig', unit_id=5))
+session.add(Vocab(english='apple', german='Apfel', unit_id=3))"""
 
 session.commit()
 session.close()
+
+if unit_choice == 1:
+    vocab_word = session.query(Vocab).filter(Vocab.unit_id == unit_choice).order_by(text("RANDOM()")).first()
 
 while True:
     # Get a random vocabulary word from the table
@@ -102,3 +114,4 @@ while True:
         cont = input("Do you want to continue? (y/n) ")
         if cont.lower() != "y":
             break
+
